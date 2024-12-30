2ad22f014a0aace4e2a9e9355fedf193e8207ee9
Izaias Lima <izaiaslima356@gmail.com>
2024-12-30 18:47:11 -0300

    feat: create register account controller

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,46 @@
+import { Body, Controller, HttpCode, Post, UsePipes } from "@nestjs/common";
+import { ConflictException } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { hash } from "bcryptjs";
+import { z } from "zod";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+
+const createAccountBodySchema = z.object({
+	name: z.string(),
+	email: z.string().email(),
+	password: z.string(),
+});
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
+
+@Controller("/account")
+export class CreateAccountController {
+	constructor(private prisma: PrismaService) {}
+
+	@Post()
+	@HttpCode(201)
+	@UsePipes(new ZodValidationPipe(createAccountBodySchema))
+	async handle(@Body() body: CreateAccountBodySchema) {
+		const { name, email, password } = body;
+
+		const userAlreadExists = await this.prisma.user.findUnique({
+			where: {
+				email,
+			},
+		});
+
+		if (userAlreadExists) {
+			throw new ConflictException("User with this email already exists.");
+		}
+
+		const hashedPassword = await hash(password, 8);
+
+		await this.prisma.user.create({
+			data: {
+				name,
+				email,
+				password: hashedPassword,
+			},
+		});
+	}
+}
